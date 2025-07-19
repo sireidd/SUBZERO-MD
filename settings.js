ab29b1937e2d3aa48f6f8308b1d2ccc730de1281
sireidd <abdallahmwangi320@gmail.com>
2025-07-19 14:38:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;yZElwRgJ#rWsXqZXNBDybvUr5w9J3S7BG3FQqao4VXdggpfQxuQk", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
